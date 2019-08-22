24b7fe4a0bc202dbd667c898fb122203abb4b273
Wang HaiChao <hcwang.docker@wind.com.cn>
2019-08-22 17:33:09 +0800

    init

---
+++ es-note/1.1.2-相关产品.sh
@@ -0,0 +1,24 @@
+ES技术解析和实践
+第一章：ES入门
+  1.1 Elasticsearch
+     1.1.1 ElasticSearch的历史
+     1.1.2 相关产品
+       1.Beats:  
+              1.1 他是一个代理，将不同类型的数据发送到ES中。
+              1.2 Beats由三部分组成：FileBeat，TopBeat，PackBeat  
+                  FileBeat用于存放日志
+		  TopBeat用于收集系统基础设置数据，CPU，内存，进程信息
+		  PackBeat 网络包分析工具，统计收集网络信息
+       2.Shield：
+              为了ES提供企业级的安全，加密通信，包含整个ES的数据，收费
+       3.Watcher：
+               是ES的报警和通知工具，可以监控ES的状态。收费
+       4.Marval:
+               是ES的管理和监控工具。监测ES集群的索引和节点的活动，收费
+
+
+
+
+
+
+
