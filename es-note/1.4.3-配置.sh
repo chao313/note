8a6b7dabfad744ab3d0665f7f18dafa0a7ecfd5b
wanghaichao <m18888106873@163.com>
2019-08-24 23:31:12 +0800

    1.4.3-配置-补充完成

--- es-note/1.4.3-配置.sh
+++ es-note/1.4.3-配置.sh
@@ -26,4 +26,32 @@
          path.logs: /path/to/logs
     4.6: bootstrap.mlockall 内存分配模式
          bootstrap.mlockall: true 
-   
+    4.7: network.host 绑定网卡
+         network.host: 192.168.0.1
+    4.8: http.port http协议端口
+         http.port: 9200
+    4.9: discovery.zen.ping.unicast.hosts 开始发现新节点的IP地址
+         discovery.zen.ping.unicast.hosts: ["host1","host2"] 
+    4.10:discovery.zen.minimum_master_nodes 最多发现的主节点的个数
+         discovery.zen.minimum_master_nodes: 3
+    4.11:gateway.recover_after_nodes 当重启节点后最少启动N个节点后开始恢复
+         gateway.recover_after_nodes: 3
+    4.12:node.max_local_storage_nodes 一台机器最多启动的节点数
+         node.max_loacl_storage_nodes: 3
+    4.13:action.destructive_requires_name 当删除一个节点的时候，需要具体的索引名称
+         action.destructive_requores_name: true
+  5.索引配置说明
+    5.1 集群中配置的索引可以提供自己的设置。
+        举例:创建一个刷新间隔为5s而不是默认刷新时间 
+          请求:PUT http://127.0.0.1:9200/kimchy
+          index:refresh_interval: 5s
+    5.2 可以统一设置(所有的索引都是5s)
+        举例: 在elasticsearch.yml文件中设置 index.refresh_interval: 5s
+    5.3 也可以启动时设置:
+        elasticsearch -Des.index.refresh_interval=5s
+  6.日志配置说明
+    6.1 ES内部使用的log4j来记录系统日志
+    6.2 可以通过YAML配置方式来简化配置
+    6.4 配置文件地址为conf/logging.yml 也可以是(.yml,.yaml,.json.properties) 
+    6.5 日志比较重要，正常情况下不要禁止日志产生
+    6.6 日志每天会生成一个新的文件   
