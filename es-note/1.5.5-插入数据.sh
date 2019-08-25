70981407b1e386551611f66aa16a472f409e6602
wanghaichao <m18888106873@163.com>
2019-08-25 23:18:39 +0800

    2.1.1-创建索引

--- es-note/1.5.5-插入数据.sh
+++ es-note/1.5.5-插入数据.sh
@@ -2,3 +2,4 @@
   1.插入URL
     模板: curl -H 'content-type:application/json' -XPUT http://<ip>:<port>/<index>/type?pretty -d '{}'
     举例: curl -H 'content-type:application/json'  -XPOST http://39.107.236.187:9200/myindex/1?pretty -d '{"name":4}'
+  
